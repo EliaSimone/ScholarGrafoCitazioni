91980f0d153089388d2e1bf3dc0a9431e0baee66
EliaSimone <33875063+EliaSimone@users.noreply.github.com>
2021-03-12 04:44:31 +0100

    Add files via upload

--- gui.py
+++ gui.py
@@ -5,7 +5,7 @@ from grafocitazioni import *
 
 def search():
     paper_dict=sch.search_single_pub(e.get())
-    paper=Paper(paper_dict)
+    paper=Paper(paper_dict,50,50)
     drawPapers()
 
 def mouseClick(e):
@@ -15,16 +15,16 @@ def mouseClick(e):
             cited=sch.citedby(p.dict)
             for i in range(5):
                 c=next(cited)
-                p.addCite(c['bib']['venue'],Paper(c,p.x+50,p.y+i*50))
+                p.addCite(c['bib']['venue'],Paper(c,p.x+80,p.y+25+i*60))
             drawPapers()
             
 
 def drawPapers():
     for p in getAllPapers():
-        cs.create_oval(p.x,p.y,p.x+50,p.y+50)
-        cs.create_text(p.x+25,p.y+25,text=p.title,font="Times 12")
+        cs.create_oval(p.x,p.y,p.x+32,p.y+32)
+        cs.create_text(p.x+16,p.y+42,text=p.title,font="Tahoma 12")
         for tag, c in p.cites:
-            cs.create_line(p.x, p.y, c.x, c.y, arrow=tk.LAST)
+            cs.create_line(p.x+32, p.y+16, c.x, c.y+16, arrow=tk.LAST)
     
 window = tk.Tk()
 window.geometry("800x600")
@@ -39,8 +39,7 @@ cs.bind('<Button-1>',mouseClick)
 
 pg = ProxyGenerator()
 pg.FreeProxies()
-pg.get_next_proxy()
-sch.use_proxy(pg)
+#sch.use_proxy(pg)
 
 drawPapers()
 tk.mainloop()
